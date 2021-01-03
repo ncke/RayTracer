593fcca5a113ae511b0b4ba9dfe3fa4e1d1dd24c
Nick Everitt <nicholaseveritt@gmail.com>
2021-01-03 07:52:05 +0000

    Add functionality to points and rays

--- Sources/RayTracer/Point.swift
+++ Sources/RayTracer/Point.swift
@@ -12,3 +12,27 @@ struct Point {
     let y: Double
     let z: Double
 }
+
+extension Point {
+
+    var asVector: Vector { Vector(x, y, z) }
+
+    init(_ x: Double, _ y: Double, _ z: Double) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+
+}
+
+func + (lhs: Point, rhs: Point) -> Point {
+    Point(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
+}
+
+func - (lhs: Point, rhs: Point) -> Point {
+    Point(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z)
+}
+
+func + (lhs: Point, rhs: Vector) -> Point {
+    Point(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
+}
